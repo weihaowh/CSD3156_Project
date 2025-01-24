bfb35398f336359e23bd3d2b1a8d51357ad7e4ff
04login <eeloong.l@digipen.edu>
2025-01-25 01:10:18 +0800

    change minsdk to 30 so i can run on phone

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.personalexpensetracker"
-        minSdk = 35
+        minSdk = 30
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
